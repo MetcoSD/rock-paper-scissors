43e3e4b7d91904d7a416b2d556fbac180aedfcc0
Troy <troy.metivier@me.com>
2022-06-10 09:32:21 -0400

    added functions playRound and game

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function computerPlay(){
     let chooice;
     let num = Math.floor(Math.random() * 3) + 1;
-    console.log(num);
     if (num == 1){
         chooice = "Rock";
     }
@@ -16,8 +15,66 @@ function computerPlay(){
     return chooice;
 
  }
-console.log(computerPlay())
 
+ function playRound (playerSelection, computerSelction){
+     if (playerSelection.toLowerCase() == "rock" && computerSelction == "Scissors"){
+         let x = 1;
+         return "you Win Rock beats scissors";
+     }
+     else if  (playerSelection.toLowerCase() == "paper" && computerSelction == "Rock"){
+         let x = 1;
+         return "you win Paper beats rock ";
+     }
+     else if (playerSelection.toLowerCase() == "scissors" && computerSelction == "Paper"){
+         let x = 1;
+         return "you win Scissors beats Paper";
+     }
+     else if (playerSelection.toLowerCase() == "rock" && computerSelction == "Paper"){
+         let y = 1;
+         return "you Lose Paper beats Rock";
+     }
+     else if  (playerSelection.toLowerCase() == "paper" && computerSelction == "Scissors"){
+         let y = 1;
+         return "you loose Scissors beats Paper ";
+     }
+     else if (playerSelection.toLowerCase() == "scissors" && computerSelction == "Rock"){
+         let y = 1;
+         return "you Loose Rock beats Scissors";
+     }
+     else if (playerSelection.toLowerCase() == computerSelction.toLowerCase()){
+         let x = 0;
+         let y = 0;
+        return "That was a tie."
+     }
+     else {
+        let x = 0;
+        let y = 0;
+         return "that isn't an option"
+     }
+ }
+
+ function game() {
+    let player = 0;
+    let computer = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSecection = prompt("Choose Rock, Paper, or Scissors");
+        playRound(playerSecection, computerSelection);
+        if (x = 1){
+            player+= 1;
+        }
+        else if (y =1) {
+            computer += 1
+        }  
+
+     }
+     console.log( "Player scored " + player + " and computer scored " + computer)
+     
+
+ }
+
+
+ const computerSelection = computerPlay();
+ game ();
 
 
 
